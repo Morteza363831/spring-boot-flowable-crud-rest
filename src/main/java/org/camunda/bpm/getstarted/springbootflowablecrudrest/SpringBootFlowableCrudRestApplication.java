c8f6d0070cc1a03c1c827488f7283c9f0df9508c
ruzstock.ir <ali16mar15couples@gmail.com>
2024-09-15 16:24:59 +0330

    spring boot app initialized

---
+++ src/main/java/org/camunda/bpm/getstarted/springbootflowablecrudrest/SpringBootFlowableCrudRestApplication.java
@@ -0,0 +1,13 @@
+package org.camunda.bpm.getstarted.springbootflowablecrudrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFlowableCrudRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFlowableCrudRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/camunda/bpm/getstarted/springbootflowablecrudrest/SpringBootFlowableCrudRestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.camunda.bpm.getstarted.springbootflowablecrudrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFlowableCrudRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
