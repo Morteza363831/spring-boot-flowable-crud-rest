166be43fa9f3bfa9aa08849139d2e9b7b84ab887
ruzstock.ir <ali16mar15couples@gmail.com>
2024-09-15 16:25:35 +0330

    model mapper bean created

---
+++ src/main/java/org/camunda/bpm/getstarted/springbootflowablecrudrest/Config.java
@@ -0,0 +1,18 @@
+package org.camunda.bpm.getstarted.springbootflowablecrudrest;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MatchingStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+
+    @Bean
+    public ModelMapper getModelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
+    }
+}
